f7629ba336ae7501042c86566f92d37977fbcf79
Kevin <kcruz.learning@gmail.com>
2025-08-16 22:01:37 +0800

    Updated Controller Advice
    - Added produces param for Exception Handlers

--- src/main/java/com/microservices/core/util/http/GlobalRestControllerExceptionHandler.java
+++ src/main/java/com/microservices/core/util/http/GlobalRestControllerExceptionHandler.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import com.microservices.core.util.exceptions.InvalidInputException;
 import com.microservices.core.util.exceptions.NotFoundException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -16,13 +17,13 @@ import java.time.ZonedDateTime;
 @RestControllerAdvice
 public class GlobalRestControllerExceptionHandler {
 
-    @ExceptionHandler(NotFoundException.class)
+    @ExceptionHandler(value = NotFoundException.class, produces = {MediaType.APPLICATION_JSON_VALUE})
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public @ResponseBody HttpErrorInfo handleNotFoundException(ServerHttpRequest request, NotFoundException nfe) {
         return buildHttpErrorInfo(HttpStatus.NOT_FOUND, request, nfe);
     }
 
-    @ExceptionHandler(InvalidInputException.class)
+    @ExceptionHandler(value = InvalidInputException.class, produces = {MediaType.APPLICATION_JSON_VALUE})
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public @ResponseBody HttpErrorInfo handleInvalidInputException(ServerHttpRequest request, InvalidInputException iie) {
         return buildHttpErrorInfo(HttpStatus.BAD_REQUEST, request, iie);
@@ -36,7 +37,7 @@ public class GlobalRestControllerExceptionHandler {
         return new HttpErrorInfo(ZonedDateTime.now(), httpStatus, message, path);
     }
 
-    @ExceptionHandler(Exception.class)
+    @ExceptionHandler(value = Exception.class, produces = {MediaType.APPLICATION_JSON_VALUE})
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public @ResponseBody HttpErrorInfo handleException(ServerHttpRequest request, Exception ex) {
         return buildHttpErrorInfo(HttpStatus.INTERNAL_SERVER_ERROR, request, ex);
